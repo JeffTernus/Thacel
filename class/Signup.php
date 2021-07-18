104bb603ba5d004ca66eff15778c640b3fc29060
GBANA <gbana.its.me@gmail.com>
2021-07-18 17:10:17 +0900

    User Validation

--- class/Signup.php
+++ class/Signup.php
@@ -1,15 +1,34 @@
 <?php
 
-class Signup {
+class Signup
+{
 	private $error = "";
 
-	public function evaluate($data) {
+	public function evaluate($data)
+	{
 		foreach ($data as $key => $value) {
 			if (empty($value)) {
 				$this->error = $this->error . $key . " is empty!<br>";
 			}
+			if ($key == "email") {
+				if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $value)) {
+					$this->error =
+						$this->error . "Please enter vaild Email<br>";
+				}
+			}
+			if ($key == "first_name") {
+				if (is_numeric($value)) {
+					$this->error =
+						$this->error . "First name can't be a number<br>";
+				}
+			}
+			if ($key == "last_name") {
+				if (is_numeric($value)) {
+					$this->error =
+						$this->error . "Last name can't be a number<br>";
+				}
+			}
 		}
-
 		if ($this->error == "") {
 			// There's no error
 			$this->create_user($data);
@@ -18,12 +37,13 @@ class Signup {
 		}
 	}
 
-	public function create_user($data) {
-		$first_name = $data['first_name'];
-		$last_name = $data['last_name'];
-		$gender = $data['gender'];
-		$email = $data['email'];
-		$password = $data['password'];
+	public function create_user($data)
+	{
+		$first_name = $data["first_name"];
+		$last_name = $data["last_name"];
+		$gender = $data["gender"];
+		$email = $data["email"];
+		$password = $data["password"];
 
 		// create the information
 		$url_address = strtolower($first_name) . "." . strtolower($last_name);
@@ -35,16 +55,16 @@ class Signup {
 		$DB = new Database();
 		$DB->save($query);
 	}
-	private function create_userid() {
+	private function create_userid()
+	{
 		$length = rand(4, 19);
 		$number = "";
 
-		for($i=0; $i < $length; $i++) {
+		for ($i = 0; $i < $length; $i++) {
 			$new_rand = rand(0, 9);
 			$number = $number . $new_rand;
 		}
 
 		return $number;
 	}
-
 }
--- signup.php
+++ signup.php
@@ -1,26 +1,26 @@
 <?php
-	include("class/connect.php");
-	include("class/Signup.php");
+include "class/connect.php";
+include "class/Signup.php";
 
-	$first_name = "";
-	$last_name = "";
-	$gender = "";
-	$email = "";
+$first_name = "";
+$last_name = "";
+$gender = "";
+$email = "";
 
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {	
-		$signup = new Signup();
-		$result = $signup->evaluate($_POST);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$signup = new Signup();
+	$result = $signup->evaluate($_POST);
 
-		if ($result != "") {
-			echo "<h4 style='color: red;'>The Error occured</h4>";
-			echo $result;
-			echo "<br>";
-		}
-		$first_name = $_POST['first_name'];
-		$last_name = $_POST['last_name'];
-		$gender = $_POST['gender'];
-		$email = $_POST['email'];
+	if ($result != "") {
+		echo "<h4 style='color: red;'>The Error occured</h4>";
+		echo $result;
+		echo "<br>";
 	}
+	$first_name = $_POST["first_name"];
+	$last_name = $_POST["last_name"];
+	$gender = $_POST["gender"];
+	$email = $_POST["email"];
+}
 ?>
 
 <!DOCTYPE html>
@@ -33,17 +33,17 @@
 </head>
 <body>
 	<form method="POST" action="">
-		<input value="<?php echo $first_name ?>" name="first_name" type="text" id="text" placeholder="first name"><br><br>
-		<input value="<?php echo $last_name ?>" name="last_name" type="text" id="text" placeholder="Last name"><br><br>
+		<input value="<?php echo $first_name; ?>" name="first_name" type="text" id="text" placeholder="first name"><br><br>
+		<input value="<?php echo $last_name; ?>" name="last_name" type="text" id="text" placeholder="Last name"><br><br>
 
 		<span style="font-weight: bold;">Gender:</span><br>
 		<select id="text" name="gender">
-			<option><?php echo $gender ?></option>
+			<option><?php echo $gender; ?></option>
 			<option>Male</option>
 			<option>Female</option>
 		</select>
 		<br><br>
-		<input type="<?php echo $email ?>" name="email" type="text" id="text" placeholder="Email"><br><br>
+		<input type="<?php echo $email; ?>" name="email" type="text" id="text" placeholder="Email"><br><br>
 
 		<input name="password" type="password" id="text" placeholder="Password"><br><br>
 		<input name="password2" type="password" id="text" placeholder="Retype Password"><br><br>
